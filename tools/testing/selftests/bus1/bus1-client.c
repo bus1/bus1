b51d785990584c6ae627e8ac11076b3f68197566
Kay Sievers <kay@vrfy.org>
2016-04-04 14:00:41 +0200

    tests: client - make send arguments arguments const

--- tools/testing/selftests/bus1/bus1-client.c
+++ tools/testing/selftests/bus1/bus1-client.c
@@ -292,10 +292,10 @@ _public_ uint64_t bus1_client_slice_to_offset(struct bus1_client *client,
 }
 
 _public_ int bus1_client_send(struct bus1_client *client,
-			      uint64_t *destinations, size_t n_destinations,
-			      struct iovec *vecs, size_t n_vecs,
-			      uint64_t *handles, size_t n_handles,
-			      int *fds, size_t n_fds)
+			      const uint64_t *destinations, size_t n_destinations,
+			      const struct iovec *vecs, size_t n_vecs,
+			      const uint64_t *handles, size_t n_handles,
+			      const int *fds, size_t n_fds)
 {
 	struct bus1_cmd_send send = {
 		.flags = n_destinations > 1 ? BUS1_SEND_FLAG_CONTINUE : 0,
--- tools/testing/selftests/bus1/bus1-client.h
+++ tools/testing/selftests/bus1/bus1-client.h
@@ -65,10 +65,10 @@ uint64_t bus1_client_slice_to_offset(struct bus1_client *client,
 				     const void *slice);
 
 int bus1_client_send(struct bus1_client *client,
-		     uint64_t *destinations, size_t n_destinations,
-		     struct iovec *vecs, size_t n_vecs,
-		     uint64_t *handles, size_t n_handles,
-		     int *fds, size_t n_fds);
+		     const uint64_t *destinations, size_t n_destinations,
+		     const struct iovec *vecs, size_t n_vecs,
+		     const uint64_t *handles, size_t n_handles,
+		     const int *fds, size_t n_fds);
 
 /* inline helpers */
 
