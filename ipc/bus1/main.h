365d18c1e5f446440eb560415a8e8fdd75ed1c5e
David Herrmann <dh.herrmann@gmail.com>
2016-04-21 12:29:42 +0200

    main: add introductory comment
    Add a short introduction to main.h with an overview over the bus1.ko
    system. It only describes the intentions of bus1.ko, and what purpose the
    kernel module serves.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- ipc/bus1/main.h
+++ ipc/bus1/main.h
@@ -13,7 +13,41 @@
 /**
  * DOC: Bus1 Overview
  *
- * XXX
+ * The bus1.ko module is a local IPC technology which provides a decentralized
+ * infrastructure to share objects between local peers. The main building
+ * blocks are nodes and handles. Nodes represent objects of a local peers,
+ * while handles represent descriptors that point to a node. Nodes can be
+ * created and destroyed by any peer, and they will always remain owned by
+ * their respective creator. Handles, on the other hand, are used to refer to
+ * nodes and can be passed around with messages as auxiliary data. Whenever a
+ * handle is transferred, the receiver will get its own handle allocated,
+ * pointing to the same node as the original handle.
+ *
+ * Any peer can send messages directed at one of their handles. This will
+ * transfer the message to the owner of the node the handle points to. If a
+ * peer does not posess a handle to a given node, it will not be able to send a
+ * message to that node. That is, handles provide exclusive access management.
+ * Anyone that somehow acquired a handle to a node is privileged to further
+ * send this handle to other peers. As such, access management is transitive.
+ * Once a peer acquired a handle, it cannot be revoked again. However, a node
+ * owner can, at anytime, destroy a node. This will effectively unbind all
+ * existing handles to that node on any peer, notifying each one of the
+ * destruction.
+ *
+ * Unlike nodes and handles, peers cannot be addresses directly. In fact, peers
+ * are completely unconnected entities in bus1.ko. A peer is merely an anchor
+ * of a set of nodes and handles, including an incoming message queue for any
+ * of those. Whether multiple nodes are all part of the same set (and as such
+ * the same peer), or part of different sets, does not affect the remote view
+ * of those. Peers solely exist as management entity and command dispatcher to
+ * local processes.
+ *
+ * The set of actors on a system is completely decentralized. There is no
+ * global component involved that provides a central registry or discovery
+ * mechanism. Furthermore, communication between peers only involves those
+ * peers, and does not affect any other peer in any way. No global
+ * communication lock is taken. However, any communication is still globally
+ * ordered, including unicasts, multicasts, and notifications.
  */
 
 /**
