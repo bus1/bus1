10b291af598d3d0b7614c6386bfcefc45d12153e
Tom Gundersen <teg@jklm.no>
2016-08-17 20:46:54 +0200

    tests: hive - instantiate handles to siblings
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- tools/testing/selftests/bus1/test-hive.c
+++ tools/testing/selftests/bus1/test-hive.c
@@ -13,14 +13,20 @@
 #include <sys/prctl.h>
 #include "test.h"
 
-#define N_CHILDREN (900)
+#define N_CHILDREN (1000)
+#define N_SIBLINGS (100)
+
+struct bus1_child {
+	struct bus1_peer *peer;
+	uint64_t siblings[N_SIBLINGS];
+};
 
 static void *child_thread(void *p) {
-	struct bus1_peer *child = p;
+	struct bus1_child *child = p;
 
 	assert(child);
 
-	bus1_peer_free(child);
+	bus1_peer_free(child->peer);
 
 	return NULL;
 }
@@ -28,7 +34,7 @@ static void *child_thread(void *p) {
 int test_hive(void)
 {
 	struct bus1_peer *parent;
-	struct bus1_peer *children[N_CHILDREN];
+	struct bus1_child children[N_CHILDREN];
 	uint64_t child_handles[N_CHILDREN];
 	size_t n_children = 0;
 	unsigned int i;
@@ -41,13 +47,33 @@ int test_hive(void)
 		uint64_t node =
 			BUS1_NODE_FLAG_MANAGED | BUS1_NODE_FLAG_ALLOCATE;
 
-		r = bus1_peer_new_from_path(children + i, test_path);
+		r = bus1_peer_new_from_path(&children[i].peer, test_path);
 		assert(r >= 0);
 
 		child_handles[i] = BUS1_HANDLE_INVALID;
-		r = bus1_peer_handle_transfer(children[i], parent, &node,
-					      child_handles + i);
+		r = bus1_peer_handle_transfer(children[i].peer,
+					      parent,
+					      &node,
+					      &child_handles[i]);
 		assert(r >= 0);
+	}
+
+	for (i = i; i < N_CHILDREN; i ++) {
+		unsigned int j;
+
+		for (j = 0; j < N_SIBLINGS; j ++) {
+			unsigned int k = (i + j + 1) % N_CHILDREN;
+
+			children[i].siblings[j] = BUS1_HANDLE_INVALID;
+
+			r = bus1_peer_handle_transfer(parent,
+						      children[i].peer,
+						      &child_handles[k],
+						      &children[i].siblings[j]);
+			if (r < 0)
+				fprintf(stderr, "%d\n", r);
+			assert(r >= 0);
+		}
 	}
 
 	for (i = 0; i < N_CHILDREN; i ++) {
@@ -60,7 +86,7 @@ int test_hive(void)
 		r = pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
 		assert(r == 0);
 
-		r = pthread_create(&t, &a, child_thread, children[i]);
+		r = pthread_create(&t, &a, child_thread, &children[i]);
 		assert(r == 0);
 
 		++ n_children;
