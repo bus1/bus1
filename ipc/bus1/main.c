0fe54d4223da5da0c69087a69c804fb1fb97c9eb
David Herrmann <dh.herrmann@gmail.com>
2016-09-09 14:22:49 +0200

    build: update copyright notice
    Update the copyright notice to the less verbose form, which we also use
    in the other projects. Reduces the number of explicit mentions of the
    project.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- ipc/bus1/main.c
+++ ipc/bus1/main.c
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 2013-2016 Red Hat, Inc.
  *
- * bus1 is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation; either version 2.1 of the License, or (at
  * your option) any later version.
  */
